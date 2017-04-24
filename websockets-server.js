0417e172e275c30174d75ffdb5cb6588c031fff2
Felipe Perez <phillip.perez44@gmail.com>
2017-04-24 10:46:29 -0700

    updated but still error in chap 17

--- websockets-server.js
+++ websockets-server.js
@@ -4,22 +4,21 @@ var port = 3001;
 var ws = new WebSocketServer({
   port: port
 });
-
 var messages = [];
 
 console.log('websockets server started');
 
-ws.on('connection',function(socket){
+ws.on('connection', function(socket) {
   console.log('client connection established');
 
-  messages.forEach(function(msg){
+  messages.forEach(function(msg) {
     socket.send(msg);
   });
 
-  socket.on('message',function(data){
-    console.log('message received:' + data);
+  socket.on('message', function(data) {
+    console.log('message received: ' + data);
     messages.push(data);
-    ws.clients.forEach(function(clientSocket){
+    ws.clients.forEach(function(clientSocket) {
       clientSocket.send(data);
     });
   });
