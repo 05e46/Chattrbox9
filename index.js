a2e4eb7add99027fc76c65662b0e6411190866e6
Felipe Perez <phillip.perez44@gmail.com>
2017-04-30 17:04:32 -0700

    done with chapter 15

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var http=require('http');
-var fs=require('fs');
-var path = require('path');
+var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
 
-var server = http.createServer(function(req,res){
+var handleError = function (err, res) {
+  res.writeHead(404);
+  res.end();
+};
+
+var server = http.createServer(function (req, res) {
   console.log('Responding to a request.');
-  var url = req.url;
-  var fileName = 'index.html';
-  if(url.length>1){
-    fileName = url.substring(1);
-  }
-  console.log(fileName);
-  var filePath = path.resolve(__dirname,'app',fileName);
-  fs.readFile(filePath,function(err,data){
-    res.end(data);
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function (err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.end(data);
+    }
   });
 });
-
 
 server.listen(3000);
